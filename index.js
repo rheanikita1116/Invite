5c484caf22ca9cd29d35a2370ebf74172011d942
MakPr016 <makpr0168@gmail.com>
2025-04-08 12:34:37 +0530

    Session update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
+const session = require('express-session');
 const path = require('path');
 const bodyParser = require("body-parser");
 
+const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
@@ -10,28 +11,38 @@ app.set('views', path.join(__dirname, 'app'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// ✅ Setup express-session middleware
+app.use(session({
+  secret: 'yourSecretKey123', // Change this to a strong secret in production
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.post("/submit-name", (req, res) => {
-  userName = req.body.name;
+  req.session.userName = req.body.name;
   res.redirect("/sign");
 });
 
 app.get("/sign", (req, res) => {
-  res.render("sign", { userName });
+  res.render("sign");
 });
 
 app.post("/submit-sign", (req, res) => {
-  userSignature = req.body.signature;
+  req.session.userSignature = req.body.signature;
   res.redirect("/invitation");
 });
 
 app.get("/invitation", (req, res) => {
+  const { userName, userSignature } = req.session;
   res.render("invitation", { userName, userSignature });
 });
+
 app.get("/record", (req, res) => {
+  const { userName, userSignature } = req.session;
   res.render("record", { userName, userSignature });
 });
 
